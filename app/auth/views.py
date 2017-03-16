651acb7e95a776c5071e06f5905cef035663eb0e
xibin.yue <xibin.yue@moji.com>
2017-03-16 18:25:56 +0800

    insert image

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,43 +1,44 @@
 #!/usr/bin/evn python
 # -*- coding: utf-8 -*-
-from flask import render_template,redirect,request,url_for,flash,current_app
-from flask_login import login_user,logout_user,login_required,current_user
+from flask import render_template, redirect, request, url_for, flash, current_app
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..model import User
-from .forms import LoginForm,RegistrationForm
+from .forms import LoginForm, RegistrationForm
 from .. import db
 from ..email import send_email
+
 
 @auth.before_app_request
 def before_request():
     if current_user.is_authenticated \
-        and not current_user.confirmed \
+            and not current_user.confirmed \
             and request.endpoint[:5] != 'auth.' \
-            and request.endpoint != 'static' :
+            and request.endpoint != 'static':
         return redirect(url_for('auth.unconfirmed'))
 
 
-
-@auth.route('/login',methods=['GET','POST'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(user,form.remember_me.data)
+            login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash(u'用户邮箱或密码错误')
-    return render_template('auth/login.html',form=form)
+    return render_template('auth/login.html', form=form)
+
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash(u'你已经退出登录')
-    return  redirect(url_for('main.index'))
+    return redirect(url_for('main.index'))
 
 
-@auth.route('/register',methods=['GET','POST'])
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
     app = current_app._get_current_object()
     form = RegistrationForm()
@@ -55,8 +56,7 @@ def register():
         #            'mail/new_user', user=user)
         # flash(u'你现在已经可以登录啦~')
         return redirect(url_for('auth.login'))
-    return render_template('auth/register.html',form = form)
-
+    return render_template('auth/register.html', form=form)
 
 
 @auth.route('/confirm/<token>')
@@ -70,18 +70,18 @@ def confirm(token):
         flash(u'确认链接无效或已过期。')
     return redirect(url_for('main.index'))
 
+
 @auth.route('/unconfirmed')
 def unconfirmed():
     if current_user.is_anonymous or current_user.confirmed:
         return redirect(url_for('main.index'))
     return render_template('auth/unconfirmed.html')
 
+
 @auth.route('/confirm')
 @login_required
 def resend_confirmation():
     token = current_user.generate_confirmation_token()
-    send_email(current_user.email,'确认你的账户','auth/email/confirm',
-               user = current_user,token=token)
+    send_email(current_user.email, '确认你的账户', 'auth/email/confirm',
+               user=current_user, token=token)
     return redirect(url_for('main.index'))
-
-
